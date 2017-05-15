d485904e6c2f4a5b2a0aea3d53ede78d32261fbd
hduss <delmas.theo@laposte.net>
2017-05-15 14:01:21 +0200

    add on, off and emit methods from EventEmitter

--- EventEmitter.js
+++ EventEmitter.js
@@ -2,13 +2,54 @@ function EventEmitter() {
 	
 	this.events = {};
 
-
 }
 
 
 
-EventEmitter.prototype.on(label, funtion) {}
+EventEmitter.prototype.on = function(eventName, fn) {
 
-EventEmitter.prototype.off(label, funtion) {}
+	if (typeof fn !== 'function' || typeof eventName !== 'string') return;
 
-EventEmitter.prototype.emit(label, data) {}
+	// si l'event n'existe pas, on le creer 
+	this.events[eventName] = this.events[eventName] || [];
+
+	this.events[eventName].push(fn);
+}
+
+EventEmitter.prototype.off(eventName, fn) {
+
+	// on verifie que l'event existe, si il n'existe pas on ne fait rien 
+	if (!this.events[eventName]) return;
+
+	// on parcours les fonctions de l'event
+	for (var i = 0; i < this.events.length; i++) {
+
+		//si une de ses fonctions vaut fn
+		if (this.events[eventName][i] === fn) {
+
+			//alors on supprime l'event
+			this.events[eventName].splice(i, 1);
+
+			// puis on stoppe la boucle quand on le trouve
+			break;
+		};
+	};
+
+	
+}
+
+EventEmitter.prototype.emit(eventName, data) {
+
+	// verifie si l'event existe
+	if (this.events[eventName]) return;
+
+	//s'il existe, execute les functions associées
+	// on parcours les functions de l'event
+	this.events[eventName].forEach(function(fn) {
+
+		// on les execute avaec comme parametre les data de emit 
+		fn(data);	
+
+	});
+
+}
